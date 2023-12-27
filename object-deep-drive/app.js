7ecab7909be18e21f0849087058a2dca32361c59
asionsolver <asionsolver274@gmail.com>
2023-12-27 13:00:03 +0600

    add new folder name like object-deep-drive and add new file name like app.js

---
+++ object-deep-drive/app.js
@@ -0,0 +1,19 @@
+/**
+* store 10 students information
+* - name
+* - age
+* - email
+* - id
+*/
+
+
+// utility function to generate a random uuid
+
+function uuidv4() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random() * 16 | 0,
+          v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+  });
+}
+
